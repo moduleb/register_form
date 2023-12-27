dd4f2711e72c5a9008773bf57763dac88b6b0732
aa <t3841@yandex.ru>
2023-12-27 17:55:33 +0400

    toggle password done

--- js/script.js
+++ js/script.js
@@ -1,69 +1,90 @@
-// Получаем ссылку и поля формы
 const loginLink = document.getElementById('login-link');
 const registerLink = document.getElementById('register-link');
 const formTypeMarkup = document.getElementById('form-type-markup');
 
-const nameField = document.getElementById('name-field-block');
-const emailField = document.getElementById('email-field-block');
-const passwordField = document.getElementById('password-field-block');
+const registerFormBox = document.getElementById('register');
+const loginFormBox = document.getElementById('login');
+const forms = document.getElementsByTagName('form');
 
-const forgot_passwordLink = document.getElementById('forgot-password-link');
-const buttonLink = document.getElementById('btn');
+const registerPasswordField = document.getElementById('register-password-field');
+const loginPasswordField = document.getElementById('login-password-field');
 
+const loginOpenEyeIcon = document.getElementById("login-open-eye-icon");
+const loginCrossedEyeIcon = document.getElementById("login-crossed-eye-icon");
+
+const registerOpenEyeIcon = document.getElementById("register-open-eye-icon");
+const registerCrossedEyeIcon = document.getElementById("register-crossed-eye-icon");
+
+loginFormBox.classList.add("hidden");
+
+
+  // * Toggles the type of the password field.
+
+  function  showPassword(){
+    loginPasswordField.type = "text";
+    loginCrossedEyeIcon.style.display = "none";
+    loginOpenEyeIcon.style.display = "block";
+
+    registerPasswordField.type = "text";
+    registerOpenEyeIcon.style.display = "block";
+    registerCrossedEyeIcon.style.display = "none";
+}
+
+function hidePassword() {
+  loginPasswordField.type = "password";
+  loginCrossedEyeIcon.style.display = "block";
+  loginOpenEyeIcon.style.display = "none";
+
+  registerPasswordField.type = "password";
+  registerOpenEyeIcon.style.display = "none";
+  registerCrossedEyeIcon.style.display = "block";
+}
+
+
+  // ----------------------------------------------------------------
 
 // Добавляем обработчик события на клик по ссылке Login
 loginLink.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  // form selector
   loginLink.style.color = "#FFF";
   registerLink.style.color = "#9E896A";
-  buttonLink.style.backgroundColor = "#9E896A";
-  buttonLink.innerHTML = "Login";
-  formTypeMarkup.style.right = "-75px"
-  e.preventDefault(); // Отменяем действие по умолчанию (переход по ссылке)
+  formTypeMarkup.style.right = "171px"
 
-  // Изменяем поля формы на имя и пароль
-  nameField.style.display = 'block';
-  passwordField.style.display = 'block';
-  emailField.style.display = 'none';
-  forgot_passwordLink.style.display = 'block';
+  // form visibility
+  loginFormBox.classList.remove("hidden");
+  registerFormBox.classList.add("hidden");
+
+  // reset forms
+  for (var i = 0; i < forms.length; i++) {
+    forms[i].reset()
+  }
+
+  // password fields type reset
+  hidePassword()
 });
 
-
 // Добавляем обработчик события на клик по ссылке Register
-registerLink.addEventListener('click', function(e) {
+registerLink.addEventListener('click', function(e) {  
+    e.preventDefault(); 
+
+    // form selector
     registerLink.style.color = "#FFF";
     loginLink.style.color = "#9E896A";
-    buttonLink.style.backgroundColor = "#553E19";
-    buttonLink.innerHTML = "Register";
-    formTypeMarkup.style.right = "-234px"
-    e.preventDefault(); // Отменяем действие по умолчанию (переход по ссылке)
-  
-    // Изменяем поля формы на имя и пароль
-    nameField.style.display = 'block';
-    passwordField.style.display = 'block';
-    emailField.style.display = 'block';
-    forgot_passwordLink.style.display = 'none';
+    formTypeMarkup.style.right = "12px"
+
+    // form visibility
+    loginFormBox.classList.add("hidden");
+    registerFormBox.classList.remove("hidden");
+
+    // reset forms
+    for (var i = 0; i < forms.length; i++) {
+    forms[i].reset()
+    }
+    // password fields type reset
+    hidePassword()
   });
 
 
-  // * Toggles the visibility of the password field.
 
-const showPasswordIcon = document.getElementById('show-password');
-const hidePasswordIcon = document.getElementById('hide-password');
-
-
-
-function togglePassword() {
-  var passwordField = document.getElementById("password-field");
-  var showPasswordIcon = document.getElementById("show-password");
-  var hidePasswordIcon = document.getElementById("hide-password");
-  
-  if (passwordField.type === "password") {
-    passwordField.type = "text";
-    showPasswordIcon.style.display = "block";
-    hidePasswordIcon.style.display = "none";
-  } else {
-    passwordField.type = "password";
-    showPasswordIcon.style.display = "none";
-    hidePasswordIcon.style.display = "block";
-  }
-}
